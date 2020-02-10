e9e7b670bd99391d5121b2a72b3a80811bb073bc
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-10 20:07:39 +0200

    update

--- sql_data/db_operations.py
+++ sql_data/db_operations.py
@@ -13,7 +13,15 @@ class db_operations():
     def insert(self, db_object, value):
 
         db_object.dataset.append(value)
-    
-    def update(self, db_object, attribute_for_editing):
-        
-        pass
+
+    def update(self, db_object, selection_attribute, old_data, insertion_attribute, new_data):
+
+        for value in db_object.dataset:
+            if value[selection_attribute] == old_data:
+                value[insertion_attribute] = new_data
+
+    def delete(self, db_object, selection_attribute, old_data, to_remove_data):
+
+        for value in db_object.dataset:
+            if value[selection_attribute] == old_data:
+                del(db_object.dataset[to_remove_data - 1])
