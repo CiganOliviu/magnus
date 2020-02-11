6cd38013c107c9bd00a40e2207060b1df204660b
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-02-11 18:22:26 +0200

    update

--- sql_data/db_operations.py
+++ sql_data/db_operations.py
@@ -15,31 +15,64 @@ class templates():
         created_date = datetime.date.today()
         datetime_list_template.append("Database updated at " + str(created_date))
 
+    def __get_size(self, dict_db_object) -> str:
+
+        to_count_data = ""
+
+        for value in dict_db_object:
+            to_count_data += str(dict_db_object[value])
+            to_count_data += "---"
+
+        return to_count_data
+
+    def draw_data_border(self, list_arg):
+
+        compressed_data = self.__get_size(list_arg)
+
+        for i in range(len(list(compressed_data))):
+            print("-", end="")
+
 class db_operations():
 
     def __init__(self):
         super(db_operations, self).__init__()
 
-    def select(self, db_object):
+    def select_from_table(self, db_object):
 
         for value in db_object.dataset:
             print(value)
 
-    def insert(self, db_object, value):
+    def insert_in_table(self, db_object, value):
 
         db_object.dataset.append(value)
 
-    def update(self, db_object, selection_attribute, old_data, insertion_attribute, new_data):
+    def update_in_table(self, db_object, selection_attribute, old_data, insertion_attribute, new_data):
 
         for value in db_object.dataset:
             if value[selection_attribute] == old_data:
                 value[insertion_attribute] = new_data
 
-    def delete(self, db_object, selection_attribute, old_data, to_remove_data):
+    def delete_from_table(self, db_object, selection_attribute, old_data, to_remove_data):
 
         for value in db_object.dataset:
             if value[selection_attribute] == old_data:
                 del(db_object.dataset[to_remove_data - 1])
+
+    def describe(self, db_object):
+
+        for value in db_object.dataset:
+            db_template = templates()
+
+            db_template.draw_data_border(value)
+            print()
+            print("|", end="")
+            for key in value:
+                print(end=" ")
+                print(value[key], end=" ")
+                print("|", end="")
+            print()
+            db_template.draw_data_border(value)
+            print()
 
     def save_activity(self, db_name):
 
--- sql_data/sampleDB/sampleDB.py
+++ sql_data/sampleDB/sampleDB.py
@@ -9,7 +9,22 @@ class sampleDB():
 	def __init__(self):
 		super(sampleDB, self).__init__()
 
+	id = "id"
+	name = "name"
+	programming_language = "programming_language"
+	license = "license"
+	
 	dataset = [
-		{},
-		{},
+		{id: 1, name: "magnus", programming_language: "Python3", license: "MIT"},
+		{id: 2, name: "misha", programming_language: "Python3", license: "MIT"},
+		{id: 3, name: "esential", programming_language: "C++17", license: "MIT"},
+		{id: 4, name: "easyPass", programming_language: "C++17", license: "MIT"},
+		{id: 5, name: "easyPass-WS", programming_language: "C++17", license: "MIT"},
+		{id: 6, name: "clean-systems", programming_language: "C++17", license: "MIT"},
+		{id: 7, name: "algo-data-structures", programming_language: "C++17", license: "MIT"},
 	]
+
+db_actions = db_operations()
+
+db_actions.describe(sampleDB)
+db_actions.save_activity("sampleDB")
