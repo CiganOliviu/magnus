917f8a1311a3b2ed0f9dca7ef266e5d6cf3a5820
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-03-26 18:05:32 +0200

    Update books database

--- sql_data/books/toReadBooks.py
+++ sql_data/books/toReadBooks.py
@@ -157,6 +157,17 @@ class toReadBooks():
 		{id: 134, book_title: "Your Successful Real Estate Career", author: "Kenneth W. Edwards", want_level: 10, status: "owned", done_to_read: False},
 		{id: 135, book_title: "Building Big Profits in Real Estate: A Guide for The New Investor", author: "Suzanne Caplan", want_level: 8, status: "owned", done_to_read: False},
 		{id: 136, book_title: "Secure Your Financial Future Investing in Real Estate", author: "Martin Stone", want_level: 10, status: "owned", done_to_read: False},
+		{id: 137, book_title: "Awaken the Giant Within How to Take Immediate Control of Your Mental, Emotional, Physical and Financial Destiny", author: "Tony Robbins", want_level: 9, status: "owned", done_to_read: False},
+		{id: 138, book_title: "Civilization and Its Discontents", author: "Freud Sigmund", want_level: 7, status: "owned", done_to_read: False},
+		{id: 139, book_title: "Compelling People The Hidden Qualities That Make Us Influential", author: "John Neffinger", want_level: 10, status: "owned", done_to_read: False},
+		{id: 140, book_title: "Contagious Why Things Catch On", author: "Berger Jonah", want_level: 8, status: "owned", done_to_read: False},
+		{id: 141, book_title: "Eat the Yolks Discover Paleo, fight food lies, and reclaim your health", author: "Wolfe", want_level: 8, status: "owned", done_to_read: False},
+		{id: 142, book_title: "King of Capital The Remarkable Rise, Fall, and Rise Again of Steve Schwarzman and Blackstone", author: "David Carey", want_level: 6, status: "owned", done_to_read: False},
+		{id: 143, book_title: "On the Heights of Despair", author: "E. M. Cioran", want_level: 10, status: "owned", done_to_read: False},
+		{id: 144, book_title: "Red notice a true story of high finance, murder, and one mans fight for justice", author: "Bill Browder", want_level: 8, status: "owned", done_to_read: False},
+		{id: 145, book_title: "The End of Faith Religion, Terror, and the Future of Reason", author: "Sam Harris", want_level: 9, status: "owned", done_to_read: False},
+		{id: 146, book_title: "The Moral Landscape How Science Can Determine Human Values", author: "Sam Harris", want_level: 9, status: "owned", done_to_read: False},
+		{id: 147, book_title: "The trouble with being born", author: "E. M. Cioran", want_level: 10, status: "owned", done_to_read: False},
 	]
 
 db_actions = db_operations()
