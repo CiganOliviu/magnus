c4069c1d175574bfb42378c04a0d52595288a293
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-06-28 18:11:22 +0300

    Update CurrentReadingCheck

--- sql_data/CurrentReadingCheck/CurrentReadingCheck.py
+++ sql_data/CurrentReadingCheck/CurrentReadingCheck.py
@@ -3,13 +3,17 @@ class CurrentReadingCheck():
 	def __init__(self):
 		super(CurrentReadingCheck, self).__init__()
 
-    id = "id"
-    book_title: "title"
-    author: "author"
-    current_page: "current page"
+	id = "id"
+	book_title = "title"
+	author = "author"
+	current_page = "current page"
     
 	dataset = [
 		{id: 1, book_title: "Refactoring", author: "Martin Flower", current_page: 100},
 		{id: 2, book_title: "Implementation Patterns", author: "Kent Beck", current_page: 143},
-    {id: 3, book_title: "Soft skills", author: "John Sonmez", current_page: 163},
+		{id: 3, book_title: "Soft skills", author: "John Sonmez", current_page: 163},
+		{id: 4, book_title: "Lean UX", author: "Jeff Gothelf", current_page: 0},
+		{id: 5, book_title: "It doesen't have to be crazy at work", author: "Jason Fried", current_page: 0},
+		{id: 6, book_title: "How to sleep well", author: "Neil Stanley", current_page: 0},
+		{id: 7, book_title: "The trouble with being born", author: "Emil Cioran", current_page: 0},
 	]
