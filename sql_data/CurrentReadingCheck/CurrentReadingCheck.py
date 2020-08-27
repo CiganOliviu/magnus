51618967833361dd19b7e460ab4baff024e05b91
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-08-27 20:10:53 +0300

    Update CurrentReadingDB

--- sql_data/CurrentReadingCheck/CurrentReadingCheck.py
+++ sql_data/CurrentReadingCheck/CurrentReadingCheck.py
@@ -12,4 +12,5 @@ class CurrentReadingCheck():
 		{id: 1, book_title: "The trouble with being born", author: "Emil Cioran", current_page: 19},
 		{id: 2, book_title: "Refactoring", author: "Martin Flower", current_page: 111},
 		{id: 3, book_title: "Soft skills", author: "John Sonmez", current_page: 163},
+		{id: 4, book_title: "The Secret Journal of Hendrik Groen", author: "Hendrik Groen", current_page: 99},
 	]
