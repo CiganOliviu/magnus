1b60a5be0ce13307efdb2be94d70295a0caf53ce
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2020-08-28 22:28:05 +0300

    Update Magnus Databases

--- sql_data/CurrentReadingCheck/CurrentReadingCheck.py
+++ sql_data/CurrentReadingCheck/CurrentReadingCheck.py
@@ -9,8 +9,8 @@ class CurrentReadingCheck():
 	current_page = "current page"
 
 	dataset = [
-		{id: 1, book_title: "The trouble with being born", author: "Emil Cioran", current_page: 19},
+		{id: 1, book_title: "The trouble with being born", author: "Emil Cioran", current_page: 40},
 		{id: 2, book_title: "Refactoring", author: "Martin Flower", current_page: 111},
 		{id: 3, book_title: "Soft skills", author: "John Sonmez", current_page: 163},
-		{id: 4, book_title: "The Secret Journal of Hendrik Groen", author: "Hendrik Groen", current_page: 99},
+		{id: 4, book_title: "The Secret Journal of Hendrik Groen", author: "Hendrik Groen", current_page: 150},
 	]
