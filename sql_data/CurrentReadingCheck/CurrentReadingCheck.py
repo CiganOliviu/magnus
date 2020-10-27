06928c96507038b37e95f0db808c3633624a2f3d
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-10-27 22:28:17 +0200

    Update CurrentReadingCheck.py

--- sql_data/CurrentReadingCheck/CurrentReadingCheck.py
+++ sql_data/CurrentReadingCheck/CurrentReadingCheck.py
@@ -9,12 +9,7 @@ class CurrentReadingCheck():
 	current_page = "current page"
 
 	dataset = [
-		{id: 1, book_title: "The trouble with being born", author: "Emil Cioran", current_page: 73},
-		{id: 2, book_title: "Refactoring", author: "Martin Flower", current_page: 200},
-		{id: 3, book_title: "Soft skills", author: "John Sonmez", current_page: 163},
-		{id: 4, book_title: "The Secret Journal of Hendrik Groen", author: "Hendrik Groen", current_page: 360},
-		{id: 5, book_title: "The book on how to invest in real estate", author: "Robert Waller", current_page: 21},
-		{id: 6, book_title: "Samurai: Razboi si onoare in japonia medievala", author: "Pamela S. Turner", current_page: 39},
-		{id: 7, book_title: "Scapa de capcana VINOVATIEI", author: "Gael Lindenfield", current_page: 100},
-		{id: 8, book_title: "Un IQ financiar mai bun", author: "Robert Kiyosaki", current_page: 175},
+		{id: 1, book_title: "Refactoring", author: "Martin Fowler", current_page: 269},
+		{id: 2, book_title: "Scoala de Afaceri", author: "Robert Kiyosaki", current_page: 50},
+		{id: 3, book_title: "Tanar si Bogat", author: "Robert Kyiosaki", current_page: 86},
 	]
