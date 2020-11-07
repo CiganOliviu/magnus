ded7af4281fcdfab3cc700a7b082abbafbecdc18
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-11-07 23:04:25 +0200

    Update CurrentReadingCheck.py

--- sql_data/CurrentReadingCheck/CurrentReadingCheck.py
+++ sql_data/CurrentReadingCheck/CurrentReadingCheck.py
@@ -9,7 +9,7 @@ class CurrentReadingCheck():
 	current_page = "current page"
 
 	dataset = [
-		{id: 1, book_title: "Refactoring", author: "Martin Fowler", current_page: 269},
-		{id: 2, book_title: "Scoala de Afaceri", author: "Robert Kiyosaki", current_page: 50},
-		{id: 3, book_title: "Tanar si Bogat", author: "Robert Kyiosaki", current_page: 86},
-	]
+		{id: 1, book_title: "Refactoring", author: "Martin Fowler", current_page: 283},
+		{id: 2, book_title: "Scoala de Afaceri", author: "Robert Kiyosaki", current_page: 100},
+		{id: 3, book_title: "Tanar si Bogat", author: "Robert Kyiosaki", current_page: 206},
+	
